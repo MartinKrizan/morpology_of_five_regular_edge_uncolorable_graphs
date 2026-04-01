0f7ff7b588eb9fa56a867e810a290b37edc4c46a
mkrizan <krizan.martin2@gmail.com>
2026-04-01 14:44:17 +0200

    k7

--- python/scripts/some_graph2.py
+++ python/scripts/some_graph2.py
@@ -16,12 +16,47 @@ def multigraph_edge_connectivity(G):
     cut_value, _ = nx.stoer_wagner(H, weight='capacity')
     return cut_value
 
+def all_pairings(arr):
+    if len(arr) % 2 != 0:
+        raise ValueError("Array length must be even.")
+
+    if not arr:
+        yield []
+        return
+
+    first = arr[0]
+
+    for i in range(1, len(arr)):
+        pair = (first, arr[i])
+        if first[:1] == arr[i][:1]:
+            continue
+        
+        remaining = arr[1:i] + arr[i+1:]
+
+        for rest in all_pairings(remaining):
+            yield [pair] + rest
+
+def isCon(pairing):
+    first = False
+    second = False
+    third = False
+    for pair in pairing:
+        if pair[0][:1] == "0" and pair[1][:1] == "1":
+            first = True
+        if pair[0][:1] == "0" and pair[1][:1] == "2":
+            second = True
+        if pair[0][:1] == "0" and pair[1][:1] == "3":
+            third = True
+        if pair[0][:1] == "1":
+            return first and second and third
+    return False
+
 def main():
     G = nx.MultiGraph()
     
-    for i in range(0,7):
-        prev = (i-1)%7
-        next = (i+1)%7
+    free_nodes = []
+    
+    for i in range(0,5):
         G.add_edge(f"{i}_0",f"{i}_1")
         G.add_edge(f"{i}_0",f"{i}_1")
         
@@ -32,28 +67,46 @@ def main():
         G.add_edge(f"{i}_2",f"{i}_3")
         
         G.add_edge(f"{i}_0",f"{i}_3")
-        
-        if i < (i+6)%7:
-            G.add_edge(f"{i}_0",f"{(i+6)%7}_0")
-        if i < (i+1)%7:
-            G.add_edge(f"{i}_0",f"{(i+1)%7}_0")
-        if i < (i+2)%7:
-            G.add_edge(f"{i}_1",f"{(i+2)%7}_3")
-        if i < (i+3)%7:
-            G.add_edge(f"{i}_2",f"{(i+3)%7}_3")
-        if i < (i+4)%7:
-            G.add_edge(f"{i}_3",f"{(i+4)%7}_1")
-        if i < (i+5)%7:
-            G.add_edge(f"{i}_3",f"{(i+5)%7}_2")
-        
-    G.add_edge("4_2", "6_3")
-    G.remove_edge("4_1","6_3")
-    draw_multigraph(G,"multi.png")
-        
-
+        free_nodes.append(f"{i}_0")
+        free_nodes.append(f"{i}_0")
+        free_nodes.append(f"{i}_1")
+        free_nodes.append(f"{i}_2")
+        free_nodes.append(f"{i}_3")
+        free_nodes.append(f"{i}_3")
     
-    print(nx.degree_histogram(G))
-    print(multigraph_edge_connectivity(G))
+    free_nodes.remove("0_0")
+    free_nodes.remove("1_3")
+    G.add_edge("0_0","1_3")
+    free_nodes.remove("0_3")
+    free_nodes.remove("2_0")
+    G.add_edge("0_3","2_0")
+    free_nodes.remove("0_1")
+    free_nodes.remove("3_0")
+    G.add_edge("0_1","3_0")
+    
+    
+    
+    cnt = 0
+    for pairing in all_pairings(free_nodes):
+        cnt+=1
+        if cnt < 0: #5M
+            continue
+        if cnt % 100000 == 0:
+            print (cnt, pairing)
+        
+       
+        H = G.copy()
+        for pair in pairing:
+            H.add_edge(pair[0], pair[1])
+        if cnt % 10000 == 0:
+            draw_multigraph(H)
+        
+        if multigraph_edge_connectivity(H) >4:
+            print(multigraph_edge_connectivity(H))
+            draw_multigraph(H)
+            print(cnt, pairing)
+            return
+        
     print(is_multigraph_edge_k_colorable(G, 5))
     
     
